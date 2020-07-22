6bae362a94c8ae0ebda2a9cdcd9295fa30ff4ae8
Rickard Andersson <gonz@severnatazvezda.com>
2020-07-23 00:02:54 +0300

    add htm extension for html content type

--- src/main.zig
+++ src/main.zig
@@ -140,7 +140,7 @@ fn determineContentType(path: []const u8) []const u8 {
         mem.endsWith(u8, path, ".cc") or
         mem.endsWith(u8, path, ".hh"))
         "text/plain"
-    else if (mem.endsWith(u8, path, ".html"))
+    else if (mem.endsWith(u8, path, ".html") or mem.endsWith(u8, path, ".htm"))
         "text/html"
     else if (mem.endsWith(u8, path, ".json"))
         "application/json"
